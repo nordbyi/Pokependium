7bfcbc0df0239e17baca5a84f90d4c8a2734349a
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 14:30:21 -0700

    Add Pokemon component

---
+++ src/Components/Pokemon/Pokemon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link
+
+const Pokemon = ({pokemon}) => {
+  const displayPokemon = pokemon.map(poke => {
+    return <Link></Link>
+  })
+}
+
+export default Pokemon
