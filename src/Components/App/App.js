a2930653b402d504b31c7fb7bf128a671c69845e
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 15:53:33 -0700

    Add useEffect to set filteredPokemon on filterQuery change

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,37 +1,42 @@
-import React, {useEffect, useState} from 'react';
-import { getPokemon } from '../../apiCalls';
-import Pokemon from '../Pokemon/Pokemon';
-import Filter from '../Filter/Filter';
+import React, { useEffect, useState } from "react";
+import { getPokemon } from "../../apiCalls";
+import Pokemon from "../Pokemon/Pokemon";
+import Filter from "../Filter/Filter";
 
 function App() {
-  const [allPokemon, setAllPokemon] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [filterQuery, setFilterQuery] = useState('')
-  const [filteredPokemon, setFilteredPokemon] = useState('')
+  const [allPokemon, setAllPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [filterQuery, setFilterQuery] = useState("");
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
 
+  useEffect(() => {
+    const filtered = allPokemon.filter((poke) =>
+      poke.name.includes(filterQuery.toLowerCase())
+    );
+    setFilteredPokemon(filtered);
+  }, [filterQuery, allPokemon]);
 
   useEffect(() => {
     const fetchPokemon = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
-        const data = await getPokemon()
-        setAllPokemon(data)
+        const data = await getPokemon();
+        setAllPokemon(data);
+      } catch {
+        setError("Failed to Fetch Pokemon");
       }
-      catch {
-        setError('Failed to Fetch Pokemon')
-      }
-      setLoading(false)
-    }
-    fetchPokemon()
-  }, [])
-  
+      setLoading(false);
+    };
+    fetchPokemon();
+  }, []);
+
   return (
     <div>
       {loading && <p>loading...</p>}
       {error && <p>{error}</p>}
-      <Filter passFilter={setFilterQuery}/>
-      <Pokemon pokemon={allPokemon}/>
+      <Filter passFilter={setFilterQuery} />
+      <Pokemon pokemon={filteredPokemon} />
     </div>
   );
 }
