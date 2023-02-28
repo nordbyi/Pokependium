dd2b56fb813e045692bc847ed9653ca94dd5b28b
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 14:29:59 -0700

    Add loading and error state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,13 +2,32 @@ import React, {useEffect, useState} from 'react';
 import { getPokemon } from '../../apiCalls';
 
 function App() {
+  const [allPokemon, setAllPokemon] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
 
   useEffect(() => {
-    getPokemon()
+    const fetchPokemon = async () => {
+      setLoading(true)
+      try {
+        const data = await getPokemon()
+        setAllPokemon(data)
+      }
+      catch {
+        setError('Failed to Fetch Pokemon')
+      }
+      setLoading(false)
+    }
+    fetchPokemon()
   }, [])
   
   return (
-    <p>Hello</p>
+    <div>
+      {loading && <p>loading...</p>}
+      {error && <p>{error}</p>}
+      <Pokemon />
+    </div>
   );
 }
 
