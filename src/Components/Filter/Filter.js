2c287d0e0b628ec8568639d8d2c2bebfbfbb33a5
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 14:58:16 -0700

    Add controlled form pattern to filter input

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,5 +1,18 @@
 import React, { useState } from "react";
 
-const Filter = () => {};
+const Filter = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div>
+      <input
+        name="filterInput"
+        placeholder="Filter Pokemon by Name"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      ></input>
+    </div>
+  );
+};
 
 export default Filter;
