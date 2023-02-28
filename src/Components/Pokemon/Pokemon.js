ff9bba47f4a0703f34af870a9e34772434496c0c
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 14:50:35 -0700

    Add styles to elements

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './pokemon.css'
 
 const Pokemon = ({ pokemon }) => {
   const displayPokemon = pokemon.map((poke) => {
     return (
       <Link key={poke.id}>
-        <img src={poke.sprites.other["official-artwork"]["front_default"]} />
+        <img className='img' src={poke.sprites.other["official-artwork"]["front_default"]} />
       </Link>
     );
   });
-  // flex div
-  return <div>{displayPokemon}</div>;
+  return <div className='container'>{displayPokemon}</div>;
 };
 
 export default Pokemon;
