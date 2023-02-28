da27667a455f709a0411840e8da084f166bb2636
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 14:12:31 -0700

    Add apiCalls file with intial fetch function

---
+++ src/apiCalls.js
@@ -0,0 +1,16 @@
+const getPokemon = async () => {
+  const InitialFetch = await fetch(
+    "http://pokeapi.co/api/v2/pokemon/?limit=251"
+  );
+  const pokemonList = await InitialFetch.json();
+  const AllPokemon = await Promise.all(
+    pokemonList.results.map(async (poke) => {
+      const resp = await fetch(poke.url);
+      return await resp.json();
+    })
+  );
+  console.log(AllPokemon);
+  return AllPokemon
+};
+
+export { getPokemon }
