0193b457a466949184006be503c64b08f1611f58
Ian Nordby <ian.c.nordby@gmail.com>
2023-02-28 14:40:03 -0700

    Add Pokemon images with unconnected link Components

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -1,10 +1,18 @@
 import React from "react";
-import Link
+import { Link } from "react-router-dom";
 
-const Pokemon = ({pokemon}) => {
-  const displayPokemon = pokemon.map(poke => {
-    return <Link></Link>
-  })
-}
+const Pokemon = ({ pokemon }) => {
+  const displayPokemon = pokemon.map((poke) => {
+    return (
+      <Link>
+        <img
+          src={poke.sprites.other["official-artwork"]["front_default"]}
+        ></img>
+      </Link>
+    );
+  });
+// flex div 
+  return <div>{displayPokemon}</div>;
+};
 
-export default Pokemon
+export default Pokemon;
