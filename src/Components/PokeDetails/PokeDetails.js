482718413dc7dcd4414f8d32c59e93eaa6e17510
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-01 14:29:30 -0700

    Add conditional rendering

--- src/Components/PokeDetails/PokeDetails.js
+++ src/Components/PokeDetails/PokeDetails.js
@@ -1,34 +1,43 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import PokemonStats from "../PokeStats/Pokestats";
 import { getPokeInfo } from "../../apiCalls";
 
-const PokeDetails = ({poke}) => {
- 
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-  const [pokeInfo, setPokeInfo] = useState({})
+const PokeDetails = ({ poke }) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [pokeInfo, setPokeInfo] = useState([]);
 
   useEffect(() => {
     const fetchPokemonInfo = async () => {
       setLoading(true);
+      setError('')
       try {
         const data = await getPokeInfo(poke.id);
-        console.log(data)
-        setPokeInfo(data);
+        console.log(data);
+        setPokeInfo(data['flavor_text_entries']);
       } catch {
         setError("Failed to Fetch Pokemon Info");
       }
       setLoading(false);
     };
     fetchPokemonInfo();
-  }, [])
-  if (!poke) return
+  }, [poke]);
+
+
+  if(!poke) return
+
   return (
     <div>
-      <img src={poke.sprites.other["official-artwork"]["front_default"]}/>
-      <PokemonStats pokemon={poke}/>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && (
+        <div>
+          <img src={poke.sprites.other["official-artwork"]["front_default"]} />
+          <PokemonStats pokemon={poke} />
+        </div>
+      )}
     </div>
-  )
+  );
 };
 
 export default PokeDetails;
