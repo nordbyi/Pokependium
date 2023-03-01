e9975f3bc8e5d51ee46955dddd29a9f248ebddd6
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-01 11:46:13 -0700

    Import and execute getPokeInfo fetch request and store return in state

--- src/Components/PokeDetails/PokeDetails.js
+++ src/Components/PokeDetails/PokeDetails.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import PokemonStats from "../PokeStats/Pokestats";
+import { getPokeInfo } from "../../apiCalls";
 
 const PokeDetails = ({poke}) => {
+ 
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [pokeInfo, setPokeInfo] = useState({})
+
+  useEffect(() => {
+    const fetchPokemonInfo = async () => {
+      setLoading(true);
+      try {
+        const data = await getPokeInfo(poke.id);
+        console.log(data)
+        setPokeInfo(data);
+      } catch {
+        setError("Failed to Fetch Pokemon Info");
+      }
+      setLoading(false);
+    };
+    fetchPokemonInfo();
+  }, [])
+  if (!poke) return
   return (
     <div>
       <img src={poke.sprites.other["official-artwork"]["front_default"]}/>
+      <PokemonStats pokemon={poke}/>
     </div>
   )
 };
