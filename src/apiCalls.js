45c0f80f6500af65fd2996310a518dd44b49adf1
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-05 13:51:32 -0700

    Add console.logs to track cypress issues

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,20 +1,23 @@
 const getPokemon = async () => {
   const InitialFetch = await fetch("http://pokeapi.co/api/v2/pokemon/?limit=251");
   const pokemonList = await InitialFetch.json();
+  console.log(pokemonList);
   const AllPokemon = await Promise.all(
     pokemonList.results.map(async (poke) => {
       const resp = await fetch(poke.url);
       return await resp.json();
     })
   );
-  console.log(AllPokemon);
+  // console.log(AllPokemon);
   return AllPokemon;
 };
 
 const getPokeInfo = async (id) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}/`)
-  const info = await response.json()
-  return info
-}
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+  );
+  const info = await response.json();
+  return info;
+};
 
 export { getPokemon, getPokeInfo };
