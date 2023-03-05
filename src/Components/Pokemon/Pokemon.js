b0d3eeb60ae1e98c8c6065d2997a310a9a517c88
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-05 12:42:13 -0700

    Add id to imgs

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -9,8 +9,8 @@ const Pokemon = ({ pokemon }) => {
   const displayPokemon = pokemon.map((poke) => {
     const path = `${poke.types.map(type => type.type.name).join('+')}/${poke.id}`
     return (
-      <Link to={path} key={poke.id}>
-        <img className='img' src={poke.sprites.other["official-artwork"]["front_default"]} />
+      <Link to={path}  key={poke.id}>
+        <img id={poke.id} className='img' src={poke.sprites.other["official-artwork"]["front_default"]} />
       </Link>
     );
   });
