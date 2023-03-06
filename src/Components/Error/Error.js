301474994e14ef782c44451d5822a1230351b676
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-05 19:33:42 -0700

    Add img and p to error component

---
+++ src/Components/Error/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <img src="https://sportshub.cbsistatic.com/i/2021/03/17/51a83182-1c70-4762-af32-73234d1a4c8b/pikachu-pokemon-1161138.jpg"/>
+      <p>Failed to Fetch Pokemon</p>
+    </div>
+  )
+}
+
+export default Error
