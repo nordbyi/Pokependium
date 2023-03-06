b276f9961b940de0523b34097bdb7eab1b2b719d
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-05 17:15:19 -0700

    Add styling

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Loading from "../Loading/Loading";
+import './AboutMe.css'
 
 const AboutMe = () => {
+  return (
+    <div className="about-container">
+      <div className="loading-spinner">
+        <Loading /> 
+      </div>
+      <div className="info">
 
+      </div>
+    </div>
+  )
 }
 
 export default AboutMe
