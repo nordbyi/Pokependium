f6299d7fd7257017d5f8e4643b71fc9c2874830e
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-05 19:33:24 -0700

    Import and conditionally render Error component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,7 @@ import PokeDetails from "../PokeDetails/PokeDetails";
 import Filter from "../Filter/Filter";
 import Nav from "../Nav/Nav";
 import Loading from "../Loading/Loading";
+import Error from "../Error/Error";
 import AboutMe from "../AboutMe/AboutMe";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
 import "./App.css";
@@ -45,7 +46,7 @@ function App() {
           <Loading />
         </div>
       )}
-      {error && <p>{error}</p>}
+      {error && <Error />}
       {!loading && !error && <Nav pokemon={allPokemon} />}
       {!loading && !error && (
         <React.Fragment>
