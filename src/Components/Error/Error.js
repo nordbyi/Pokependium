caa2dcfa2f93d31694357aa7bcfbb461f36b34e7
Ian Nordby <ian.c.nordby@gmail.com>
2023-03-05 19:35:47 -0700

    Import css and add className

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Error.css'
 
 const Error = () => {
   return (
-    <div>
+    <div className="error-container">
       <img src="https://sportshub.cbsistatic.com/i/2021/03/17/51a83182-1c70-4762-af32-73234d1a4c8b/pikachu-pokemon-1161138.jpg"/>
       <p>Failed to Fetch Pokemon</p>
     </div>
